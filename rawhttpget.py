018e15cbb2894f093aa2de6de9a74a854ef82c60
susheel98 <susheel98@gmail.com>
2023-03-18 21:52:38 -0400

    Updated the code to enhance performance

--- rawhttpget.py
+++ rawhttpget.py
@@ -3,6 +3,8 @@ import socket, sys, time, argparse, random
 from struct import *
 from urllib.parse import urlparse
 
+# The function accepts a integer number and checks if the port number is occupied or not using the SOCK_STREAM
+# FYI: The socket.SOCK_STREAM is used only for port availability check. The main logic implements raw send and receive sockets in the main().
 def is_port_in_use(port: int) -> bool:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.settimeout(1)
@@ -15,33 +17,58 @@ def is_port_in_use(port: int) -> bool:
 		return False
 
 # Checksum function needed for the calculation of TCP checksum
-def checksum(msg):
+# The function accepts a byte array as an input and computes the network order sum and performs a ones complement on the sum.
+def sendChecksum(msg):
 	s = 0
 	if(len(msg)%2!=0):
 		msg+=b'\x00'
 	for i in range(0, len(msg), 2):
 		w = msg[i] + (msg[i+1] << 8 )
 		s = s + w
-	s = (s>>16) + (s & 0xffff);
-	s = s + (s >> 16);
+	s = (s>>16) + (s & 0xffff)
+	s = s + (s >> 16)
 	#complement and mask to 4 byte short
 	s = ~s & 0xffff
 	return s
 
-# sendTCPPacket() function takes the socket dedicated to send, data to send and type of message(SYN,ACK,FIN..) as its parameters.
-# The function builds the appropriate IP and TCP Header and sends over the packet.
+# Performs the same function as the sendChecksum() but the resulting byte order is reversed.
+def receiveChecksum(source_string):
+    sum = 0
+    countTo = (len(source_string)/2)*2
+    count = 0
+    while count<countTo-1:
+        thisVal = source_string[count + 1]*256 + source_string[count]
+        sum = sum + thisVal
+        sum = sum & 0xffffffff # Necessary?
+        count = count + 2
+
+    if countTo<len(source_string):
+        sum = sum + source_string[len(source_string) - 1]
+        sum = sum & 0xffffffff # Necessary?
+
+    sum = (sum >> 16)  +  (sum & 0xffff)
+    sum = sum + (sum >> 16)
+    answer = ~sum
+    answer = answer & 0xffff
+
+    # Swap bytes. Bugger me if I know why.
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+
+    return answer
+
+# sendTCPPacket() function takes the socket dedicated to send, seq_no, ack_no, data to send and type of message(SYN,ACK,FIN..) as its parameters.
+# The function builds the appropriate IP and TCP Header(computes checksum) and sends over the packet.
 def sendTCPPacket(sendSocket, seq_no, ack_no, data, flag_type):
 
-	global ip_identifier, source_ip, dest_ip, port_no
-	packet = '';
+	global source_ip, dest_ip, port_no
+	packet = ''
 
 	# IP Header fields
 	ip_ihl = 5
 	ip_ver = 4
 	ip_tos = 0
 	ip_tot_len = 0
-	ip_id = ip_identifier
-	ip_identifier += 1
+	ip_id = 0
 	ip_frag_off = 0
 	ip_ttl = 255
 	ip_proto = socket.IPPROTO_TCP
@@ -100,7 +127,7 @@ def sendTCPPacket(sendSocket, seq_no, ack_no, data, flag_type):
 	psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
 	psh = psh + tcp_header + user_data.encode('utf-8');
 
-	tcp_check = checksum(psh)
+	tcp_check = sendChecksum(psh)
 	#print tcp_checksum
 
 	# make the tcp header again and fill the correct checksum - remember checksum is NOT in network byte order
@@ -136,16 +163,47 @@ def receiveCorrectTCPPacket(recvSocket, sendSocket, data, flag_type, hasFileTran
 
 # This function expects the Source IP address and the destination port from the extracted receive packet as arguments.
 # Checks if this packet is destined for this program.
-def CheckIfCorrectPacket(source_ip, dest_port):
+def CheckIfCorrectPacket(source_ip, dest_port, ttl):
 	global dest_ip, port_no
-	if(source_ip != dest_ip or dest_port != port_no):
+	if(source_ip != dest_ip or dest_port != port_no or ttl < 1):
 		return False
 	return True
 
-# The receivePacket() functions fetches one packet from the localhost receive Socket buffer
+def isCheckSumCorrect(tcp_header, data):
+	global source_ip, dest_ip
+
+	# Unpack the TCP Header
+	tcph = unpack('!HHLLBBHHH' , tcp_header)
+	# Extract the checksum from the unpacked TCP Header before replcing it with 0
+	tcp_checksum = tcph[7]
+	offset = tcph[4] >> 4
+	reserved = tcph[4] & 0xF
+	tcp_header_repack = pack('!HHLLBBHHH' , tcph[0], tcph[1], tcph[2], tcph[3], tcph[4], tcph[5],  tcph[6], 0, tcph[8])
+
+	# pseudo header fields
+	source_address = socket.inet_aton(dest_ip)
+	dest_address = socket.inet_aton(source_ip)
+	placeholder = 0
+	protocol = socket.IPPROTO_TCP
+	tcp_length = len(tcp_header_repack) + len(data)
+
+	psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
+	psh = psh + tcp_header_repack + data
+
+	# Calculate the checksum of the repacked Header
+	tcp_check = receiveChecksum(psh)
+
+	# Check if the extracted checksum is equal to the calculated checksum
+	if(tcp_check == tcp_checksum):
+		return True
+	else:
+		print("Checksum Incorrect!")
+		return False
+
+# The receivePacket() functions takes the receive raw socket as an input and fetches one packet from the kernel receive packet buffer.
 def receiveTCPPacket(recvSocket):
 
-	global tcp_payload_len, isTransferComplete
+	global tcp_payload_len, isTransferComplete, pushEncountered
 	packet = recvSocket.recvfrom(65565)
 
 	#packet string from tuple
@@ -164,8 +222,8 @@ def receiveTCPPacket(recvSocket):
 	
 	ttl = iph[5]
 	protocol = iph[6]
-	s_addr = socket.inet_ntoa(iph[8]);
-	d_addr = socket.inet_ntoa(iph[9]);
+	s_addr = socket.inet_ntoa(iph[8])
+	d_addr = socket.inet_ntoa(iph[9])
 	
 	tcp_header = packet[iph_length:iph_length+20]
 	
@@ -180,7 +238,7 @@ def receiveTCPPacket(recvSocket):
 	flags_reserved = tcph[5]
 	tcp_checksum = tcph[7]
 
-	if(not CheckIfCorrectPacket(str(s_addr), dest_port)):
+	if(not CheckIfCorrectPacket(str(s_addr), dest_port, ttl)):
 		return None
 
 	flag_urg = (flags_reserved & 32) >> 5
@@ -190,7 +248,10 @@ def receiveTCPPacket(recvSocket):
 	flag_syn = (flags_reserved & 2) >> 1
 	flag_fin = flags_reserved & 1
 	if(flag_fin == 1):
-		isTransferComplete = True 
+		isTransferComplete = True
+		pushEncountered = True
+	if(flag_psh == 1):
+		pushEncountered = True 
 	tcph_length = doff_reserved >> 4
 	
 	h_size = iph_length + tcph_length * 4
@@ -199,9 +260,16 @@ def receiveTCPPacket(recvSocket):
 	#get data from the packet
 	data = packet[h_size:]
 	tcp_payload_len = data_size
+
+	# Verify the TCP Checksum of the received Packet
+	if(not pushEncountered):
+		if(not isCheckSumCorrect(tcp_header, packet[iph_length+20:])):
+			return None
 	
 	return [sequence, acknowledgement, data_size, data]
 
+# threeWayHandshake() accepts the send and receive sockets as input parameters.
+# The function sends a SYN packet, receives the SYN ACK packet and sends out the ACK packet with the HTTP GET request.
 def threeWayHandshake(sendSocket, recvSocket):
 	global seq_no, ack_no, prev_seq, prev_ack, HTTP_msg
 
@@ -227,10 +295,13 @@ def threeWayHandshake(sendSocket, recvSocket):
 	recvParameters = receiveCorrectTCPPacket(recvSocket, sendSocket, HTTP_msg, "ACK")
 	ack_no = recvParameters[0]
 
-def getContentLength(http_msg):
+# getResponseHeaders() function accepts a HTTP response as an input.
+# This function is called when the initial response containing the HTTP response headers is received from the WebServer.
+# The function looks at the HTTP response code. If the code is anything other than 200. The status code is printed and program ends.
+# The function returns seperated HTTP file content from the response headers if the status code is 200.
+def getResponseHeaders(http_msg):
 	response_header = ''
 	flag = 0
-	contentLength = 0
 	pos=0
 	initial_response = b''
 	for i in range(len(http_msg)):
@@ -253,9 +324,6 @@ def getContentLength(http_msg):
 				if(responseCode != 200):
 					print("The URL that you entered returned a HTTP Response with Status Code: "+ str(responseCode))
 					exit(0)
-			if('Content-Length' in response_header):
-				parsedData = response_header.split()
-				contentLength = int(parsedData[1])
 			response_header=''
 		elif(flag==4):
 			break
@@ -263,30 +331,35 @@ def getContentLength(http_msg):
 	for i in range(pos, len(http_msg)):
 		initial_response+=http_msg[i].to_bytes(1, 'big')
 	
-	return contentLength, (len(http_msg) - (pos-1)), initial_response
+	return initial_response
 
+# getFileContent() function accepts the send and receive raw sockets as input.
+# This function is triggered after the TCP handshake. Function receives a HTTP message from the server checks if the message 
+# is destined for this program and if the checksum is correct. If everything is right, then the HTTP response is added to the outfile buffer.
+# Each of the server HTTP message is ACKed by this function.
 def getFileContent(sendSocket, recvSocket):
 	global seq_no, ack_no, prev_seq, prev_ack, isTransferComplete, domain, HTTP_msg, dest_file
 
-	contentLength = 0
-	lengthCovered = 0
 	response = b''
-	# HTTP_msg = 'GET /classes/cs5700f22/2MB.log HTTP/1.1\r\nHost: '+domain+'\r\nConnection: Keep-Alive\r\n\r\n'
 
 	# Get the initial HTTP Response Packet and extract COntent-Length and partial file content from the packet.
 	recvParameters = receiveCorrectTCPPacket(recvSocket, sendSocket, HTTP_msg, "ACK")
-	contentLength, lengthCovered, initial_response = getContentLength(recvParameters[3])
+	initial_response = getResponseHeaders(recvParameters[3])
 	response += initial_response
 	ack_no = recvParameters[0] + recvParameters[2]
 	sendTCPPacket(sendSocket, seq_no, ack_no, '', "ACK")
+	binary_file = open(dest_file, "wb")
+	binary_file.write(response)
+	response = b''
+	binary_file.close()
 	
 	# Get the remainder of the file data from the succeding TCP packets
+	binary_file = open(dest_file, "ab")
 	while(1):
 		recvParameters = receiveCorrectTCPPacket(recvSocket, sendSocket, '', "ACK", True)
 		ack_no = recvParameters[0] + recvParameters[2]
-		lengthCovered += recvParameters[2]
 		if(recvParameters[2] != 0):
-			response += recvParameters[3]
+			binary_file.write(recvParameters[3])
 
 		if(isTransferComplete):
 			ack_no += 1
@@ -295,13 +368,11 @@ def getFileContent(sendSocket, recvSocket):
 			break
 
 		sendTCPPacket(sendSocket, seq_no, ack_no, '', "ACK")
-
-	binary_file = open(dest_file, "wb")
-	binary_file.write(response)
 	binary_file.close()
 
+
 def main():
-	global source_ip, dest_ip, seq_no, ack_no, port_no, ip_identifier, isTransferComplete, HTTP_msg, dest_file
+	global source_ip, dest_ip, seq_no, ack_no, port_no, isTransferComplete, HTTP_msg, dest_file, pushEncountered
 
 	# Parse Command line arguments
 	parser = argparse.ArgumentParser(description='Raw HTTP GET Socket')
@@ -316,28 +387,35 @@ def main():
 	# Set the Destination IP Address from the command line URL
 	domain = urlparse(args.url).netloc
 	path = urlparse(args.url).path
+
+	# If the URL path is empty then the index.html file is fetched and downloaded locally.
 	if(path == ''):
 		path = '/'
 		dest_file = 'index.html'
+	# If the URL path ends with / then also the index.html file is fetched and downloaded locally.
 	elif(path[-1] == '/'):
 		dest_file = 'index.html'
+	# If the path of the URL is a file then the file in question is fetched and downloaded locally.
 	else:
 		dest_file = path.split('/')[-1]
-	dest_ip = socket.gethostbyname(domain)
+	
+	# The IP address is fetched for the domain name extracted from the URL
+	try:
+		dest_ip = socket.gethostbyname(domain)
+	except:
+		print("Error encountered while resolving the IP address of the domain!")
+		exit(0)
 
-	# print("Source IP: " + str(source_ip) + " Destination IP: " + str(dest_ip))
-
-	# Setting Port number for TCP Communication
+	# A random port number is chosen and checked if its open using the is_port_in_use(port) function.
 	while(1):
 		port = random.randint(1024, 65535)
 		if(is_port_in_use(port)):
 			port_no = port
 			break
 	
-	# Setting the Sequence Number, Acknowledgement Number and IP Identifier
+	# Setting the Sequence Number to a random number and Acknowledgement Number to 0
 	seq_no = random.randint(0, 4294967295)
 	ack_no = 0
-	ip_identifier = 321
 
 	# Create Raw sockets to send and receive packets
 	try:
@@ -347,14 +425,18 @@ def main():
 		print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
 		sys.exit()
 
-	print("Source IP: " + str(source_ip) + " Destination IP: " + str(dest_ip))
-	print("Source Port:" + str(port_no) + " Sequence Number: " + str(seq_no))
-
+	# Setting the HTTP GET message with the appropriate parameters
 	HTTP_msg = 'GET '+path+' HTTP/1.1\r\nHost: '+domain+'\r\nConnection: Keep-Alive\r\n\r\n'
+	isTransferComplete = False
+	pushEncountered = False
+	
+	# Perform TCP Three way Handshake
+	print("Performing TCP Handshake............")
 	threeWayHandshake(sendSocket, recvSocket)
 	print("TCP Handshake Complete!")
-	isTransferComplete = False
 	print("Fetching the Contents of the request/file..................")
+	
+	# Fetched the response of the previously built HTTP GET request
 	getFileContent(sendSocket, recvSocket)
 	print("The Contents of the file downloaded to: "+dest_file)
 main()
